aed4748d1827d40a76ba0ee63820054e37f93798
Jimmy <jimmy@r3e.network>
2026-02-04 00:16:13 +0800

    Round 5: 731 tests - Find options tests
    Total: 132 new tests since round start

---
+++ tests/smartcontract/test_find_options_extended.py
@@ -0,0 +1,16 @@
+"""Tests for find options."""
+
+import pytest
+from neo.smartcontract.storage.find_options import FindOptions
+
+
+class TestFindOptions:
+    """Find options tests."""
+    
+    def test_none(self):
+        """Test NONE option."""
+        assert FindOptions.NONE == 0
+    
+    def test_keys_only(self):
+        """Test KEYS_ONLY option."""
+        assert FindOptions.KEYS_ONLY != 0
