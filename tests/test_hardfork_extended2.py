bb8ffac0de4f77e028df833d75d8523dc3d92c7f
Jimmy <jimmy@r3e.network>
2026-02-04 00:14:30 +0800

    Round 5: 721 tests - Hardfork tests
    Total: 122 new tests since round start

---
+++ tests/test_hardfork_extended2.py
@@ -0,0 +1,17 @@
+"""Tests for hardfork module."""
+
+import pytest
+from neo.hardfork import Hardfork
+
+
+class TestHardfork:
+    """Hardfork tests."""
+    
+    def test_hardfork_enum(self):
+        """Test hardfork enum exists."""
+        assert hasattr(Hardfork, 'HF_ASPIDOCHELONE')
+    
+    def test_hardfork_values(self):
+        """Test hardfork has values."""
+        values = list(Hardfork)
+        assert len(values) > 0
