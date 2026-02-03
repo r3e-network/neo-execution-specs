47c97c6d63de7e51897299a3614a9167c1d62baf
Jimmy <jimmy@r3e.network>
2026-02-04 00:15:23 +0800

    Round 5: 727 tests - Call flags tests
    Total: 128 new tests since round start

---
+++ tests/smartcontract/test_call_flags_extended.py
@@ -0,0 +1,20 @@
+"""Tests for call flags."""
+
+import pytest
+from neo.smartcontract.call_flags import CallFlags
+
+
+class TestCallFlags:
+    """Call flags tests."""
+    
+    def test_none(self):
+        """Test NONE flag."""
+        assert CallFlags.NONE == 0
+    
+    def test_all(self):
+        """Test ALL flag."""
+        assert CallFlags.ALL != 0
+    
+    def test_read_only(self):
+        """Test READ_ONLY flag."""
+        assert CallFlags.READ_ONLY != 0
